2f0726ba8dedc2a1d3d5126434a7ac4fe7d3ab02
Wei Hsu <wei@airtime.com>
2013-01-24 15:18:46 -0800

    take out extraneous calls

--- src/itest/core.clj
+++ src/itest/core.clj
@@ -1,61 +1,43 @@
 (ns itest.core
-  (:use compojure.core)
+  (:use [compojure.core]
+        [clojure.java.browse :only [browse-url]])
   (:require [clojure.tools.nrepl.server :as nrepl-server]
             [clojure.tools.nrepl :as repl]
             [cljs.repl.browser]
-            [clj-http.client :as http]
             [clojure.pprint]
             [ring.util.serve :as ring-serve]
             [cemerick.piggieback :as pback]
             [compojure.handler :as handler]
             [compojure.route :as route]
-            [itest.util :as util]
-            ))
-
-(comment
-  (defn start-server []
-    (server/start-server
-      :handler (server/default-handler #'pback/wrap-cljs-repl))))
-
-(def counter (atom 0))
+            [itest.util :as util]))
 
 (defroutes app-routes
-           (GET "/inc" [] (str (swap! counter inc)))
-           (route/resources "/")
-           (route/not-found "Not Found"))
-
-(def app
-  (handler/site app-routes))
+           (route/resources "/"))
 
 (defn start-web []
-  (ring-serve/serve app))
+  (ring-serve/serve-headless (handler/site app-routes)))
 
-(def test-cmds
-  '(do
-     (ns itest.core)
-     (load "core")
-     (http/get "http://0.0.0.0:3000/inc")
-     (http/get "http://0.0.0.0:3000/inc")
-     (http/get "http://0.0.0.0:3000/inc")
-     (http/get "http://0.0.0.0:3000/inc")
-     @counter
-     ))
+(def upgrade-to-cljs-repl
+ '(cemerick.piggieback/cljs-repl
+       :repl-env (doto (cljs.repl.browser/repl-env :port 9000) cljs.repl/-setup)))
 
 (defn -main [& m]
+  (println "**************** Started webserver. ****************")
   (start-web)
-  (println "started web.")
-  ;(http/get "http://0.0.0.0:3000/index.html")
-  (let [server (nrepl-server/start-server
-                 :handler (nrepl-server/default-handler #'pback/wrap-cljs-repl))]
-    (println "started server repl.waiting 5 sec for connection...")
-    (Thread/sleep 5000)
-    (println "connecting to our js repl")
-    (with-open [conn (repl/connect :port 9000)]
-      (println
-        (->
-          (repl/client conn 1000)
-          (repl/message {:op :eval :code "(js/alert \"yay\")"})
-          doall
-          clojure.pprint/pprint)))
-    (nrepl-server/stop-server server))
+
+  (println "**************** Starting JVM REPL. ****************")
+  (nrepl-server/start-server
+    :handler (nrepl-server/default-handler #'pback/wrap-cljs-repl)
+    :port 7888)
+
+  (println "**************** Upgrading the REPL to a browser-REPL. ****************")
+  ;; produces this error:
+  ;; java.lang.IllegalStateException: Can't change/establish root binding of: *cljs-ns* with set
+  (with-open [conn (repl/connect :port 7888)]
+    (println
+      (->
+        (repl/client conn 1000)
+        (repl/message {:op :eval :code (str upgrade-to-cljs-repl)})
+        clojure.pprint/pprint)))
+  (browse-url "http://localhost:3000/index.html")
   (System/exit 0))
